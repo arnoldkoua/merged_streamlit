a7de7530257642a00be4b108796e837078a317a9
Kouassi David-Arnold Kouadio <48394812+arnoldkoua@users.noreply.github.com>
2023-03-12 18:40:59 +0100

    Add files via upload

--- excel_merged.py
+++ excel_merged.py
@@ -13,27 +13,36 @@ st.write("Upload your Excel files below. The files will be merged into a single 
 # Allow user to upload files
 uploaded_files = st.file_uploader("Upload Excel files", type=["xls", "xlsx"], accept_multiple_files=True)
 
-# Merge the uploaded files into a single dataframe
+# Display the uploaded files to the user
 if uploaded_files:
-    all_dataframes = []
-    variable_names = None # Initialize variable_names
+    st.write("Uploaded Files")
     for file in uploaded_files:
-        df = pd.read_excel(file)
-        if variable_names is None:
-            variable_names = set(df.columns)
-        elif variable_names != set(df.columns):
-            st.warning("The uploaded files have different variables. The merge may result in unexpected data. Please upload files with the same variables.")
-            break
-        all_dataframes.append(df)
+        st.write(file.name)
+
+# Merge the uploaded files into a single dataframe when the user clicks the button
+if st.button("Merge"):
+    if uploaded_files:
+        all_dataframes = []
+        variable_names = None # Initialize variable_names
+        for file in uploaded_files:
+            df = pd.read_excel(file)
+            if variable_names is None:
+                variable_names = set(df.columns)
+            elif variable_names != set(df.columns):
+                st.warning("The uploaded files have different variables. The merge may result in unexpected data. Please upload files with the same variables.")
+                break
+            all_dataframes.append(df)
+        else:
+            merged_dataframe = pd.concat(all_dataframes)
+
+            # Display the merged dataframe to the user
+            st.write("Merged Dataframe")
+            st.write(merged_dataframe)
+
+            # Allow user to download the merged file
+            with st.spinner('Downloading...'):
+                merged_dataframe.to_excel("merged_file.xlsx", index=False)
+            st.success('File ready to be downloaded!')
+            st.download_button(label="Download Merged File", data=open("merged_file.xlsx", 'rb').read(), file_name="merged_file.xlsx", mime="application/vnd.ms-excel")
     else:
-        merged_dataframe = pd.concat(all_dataframes)
-
-        # Display the merged dataframe to the user
-        st.write("Merged Dataframe")
-        st.write(merged_dataframe)
-
-        # Allow user to download the merged file
-        with st.spinner('Downloading...'):
-            merged_dataframe.to_excel("merged_file.xlsx", index=False)
-        st.success('Download Completed!')
-        st.download_button(label="Download Merged File", data=open("merged_file.xlsx", 'rb').read(), file_name="merged_file.xlsx", mime="application/vnd.ms-excel")
+        st.warning("Please upload at least one file.")
